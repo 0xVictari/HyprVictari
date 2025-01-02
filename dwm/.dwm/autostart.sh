f761bbeef623d201a397c4c0e67d19783f6dba24
0xVictari <you@you.you>
2025-01-02 17:39:22 -0300

    adding autolock

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 xrandr --output eDP --mode 1280x720 --auto
 #xrandr --output HDMI-A-0 --mode 1366x768
-feh --bg-scale ~/Pictures/ar0wdypsajx41.png &
+feh --bg-scale ~/Pictures/background &
 sxhkd &
 slstatus &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+xautolock -time 3 -locker "cinnamon-screensaver-command -l" \
+ -killtime 15 -killer "systemctl suspend" &
