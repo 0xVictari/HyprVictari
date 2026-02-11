ee3a858712ed817b151d449d32476002824ca09a
Francisco Casse <you@you.you>
2026-02-11 11:08:50 -0300

    uploading the recent changes in the dwm stack

---
+++ suckless/victariwm/patch/include.h
@@ -0,0 +1,26 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alternativetags.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "restartsig.h"
+#include "seamless_restart.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
